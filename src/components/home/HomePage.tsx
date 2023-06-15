d0bd49cf3bf49364c35d827a10567a35c482059b
novakvova <novakvova@gmail>
2023-06-15 20:59:27 +0300

    Begin working card view

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -1,67 +1,49 @@
 import { useEffect, useState } from "react";
 import Slider from "./Slider";
-import { ICategoryItem } from "./types";
-import { APP_ENV } from "../../env";
+import { IProductHomePage } from "./types";
 import http from "../../http_common";
-import { Link } from "react-router-dom";
 import EclipseWidget from "../common/eclipse";
+import { APP_ENV } from "../../env";
 
 const HomePage = () => {
   const [loading, setLoading] = useState<boolean>(false);
 
-  const [list, setList] = useState<ICategoryItem[]>([]);
-
-  // map - перебирає список елементів, як foreach, але при цьому він повертає розмітку (можна використовувати return)
-  // key - є обов'язковим, тому, що коли буде порівнюватися Virtual DOM і фактичний дом на сторінці 
-  //- будуть виникати помилки по відображеню елементів, бо не буде зрозуміло, як кожен із елементів відрізняється між собою
-  const viewList = list.map((item) => {
-    return (
-      <tr key={item.id}>
-        <th scope="row">{item.id}</th>
-        <td>
-          <img src={`${APP_ENV.BASE_URL}images/150_${item.image}`} alt="Якась фотка" width="75" />
-        </td>
-        <td>{item.title}</td>
-      </tr>
-    );
-  });
-
+  const [home, setHome] = useState<IProductHomePage>();
 
   useEffect(() => {
-    console.log("Working useEffect");
     setLoading(true);
-    http.get<ICategoryItem[]>(`api/categories/list`)
-      .then(resp => {
-        console.log("Server responce", resp.data); 
-        const {data} = resp;
-        setList(data);
-        setLoading(false);
-      });
-  },[]);
-  
-  console.log("Render component", APP_ENV);
+    http.get<IProductHomePage>(`api/products/search`).then((resp) => {
+      const { data } = resp;
+      //console.log("----Products---", data);
+      setHome(data);
+      setLoading(false);
+    });
+  }, []);
 
-
-  
-  
   return (
     <>
-      {loading && <EclipseWidget/>}
+      {loading && <EclipseWidget />}
       <Slider />
-      <h1 className="text-center">Головна сторінка</h1>
-      <Link to="/admin/categories/create" className="btn btn-success">Додати</Link>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Id</th>
-            <th scope="col">Фото</th>
-            <th scope="col">Назва</th>
-          </tr>
-        </thead>
-        <tbody>
-          {viewList}
-        </tbody>
-      </table>
+      <h1 className="text-center">Товари</h1>
+      <div className="row ">
+        {home?.products.map((p) => (
+          <div className="card col-md-3 h-100">
+            <img
+              style={{ width: "100%", height: "150px", objectFit: "contain" }}
+              src={`${APP_ENV.BASE_URL}images/300_${p.images[0]}`}
+              className="card-img-top"
+              alt="Козачка"
+            />
+            <div className="card-body">
+              <h5 className="card-title">{p.name}</h5>
+              <p className="card-text">{p.categoryName}</p>
+              <a href="#" className="btn btn-primary">
+                Купить
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
--- src/components/home/types.ts
+++ src/components/home/types.ts
@@ -1,7 +1,17 @@
+import { IProductItem } from "../admin/products/types";
+
 export interface ICategoryItem {
-    id: number,
-    title: string,
-    urlSlug: string,
-    priority: number,
-    image: string
+  id: number;
+  title: string;
+  urlSlug: string;
+  priority: number;
+  image: string;
+}
+
+export interface IProductHomePage {
+  products: Array<IProductItem>;
+  pages: number;
+  currentPage: number;
+  total: number;
+  categoryName: string;
 }
