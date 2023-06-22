b2c831348c56d09a8a6d8d922e524e9c4fa76a61
novakvova <novakvova@gmail>
2023-06-22 20:04:50 +0300

    Fix login info

--- src/components/auth/login/LoginPage.tsx
+++ src/components/auth/login/LoginPage.tsx
@@ -1,12 +1,12 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import {ChangeEvent, FormEvent, useState} from "react";
 import http from "../../../http_common";
 import InputGroup from "../../common/InputGroup";
-import { ILoginPage, ILoginPageError, IUser } from "./types";
+import {ILoginPage, ILoginPageError, IUser} from "./types";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "../../../helpers/setAuthToken";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { AuthUserActionType } from "../types";
+import {useDispatch} from "react-redux";
+import {useNavigate} from "react-router-dom";
+import {AuthUserActionType} from "../types";
 
 const LoginPage = () => {
 
@@ -15,6 +15,8 @@ const LoginPage = () => {
         email: "",
         password: ""
     };
+
+    const [error, setError] = useState<string>("");
 
     //При зміни значення елемента в useState компонент рендериться повторно і виводить нові значення
     const [data, setData] = useState<ILoginPage>(init);
@@ -25,28 +27,29 @@ const LoginPage = () => {
     //console.log("Дестурктуризація", {...data, password: "123456"});
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    
+
     const onSubmitHandler = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
         console.log("Ми відправляємо на сервер", data);
         http.post("api/account/login", data)
-          .then(resp=> {
-            const token = resp.data.token as string;
-            setAuthToken(token);
-            const user = jwt_decode<IUser>(token);
-            
-            dispatch({type: AuthUserActionType.LOGIN_USER, payload: user});
-            navigate("/");
-            
-          })
-          .catch(badReqeust => {
-            //Помилки, які ідуть від сервера
-            const errors = badReqeust.response.data.errors as ILoginPageError; 
-            console.log("Вхід не успішний", badReqeust.response.data);
-            console.log("Errors ", errors);
-            
-          });
+            .then(resp => {
+                const token = resp.data.token as string;
+                setAuthToken(token);
+                const user = jwt_decode<IUser>(token);
+
+                dispatch({type: AuthUserActionType.LOGIN_USER, payload: user});
+                navigate("/");
+
+            })
+            .catch(badReqeust => {
+                //Помилки, які ідуть від сервера
+                const errors = badReqeust.response.data.errors as ILoginPageError;
+                //console.log("Вхід не успішний", badReqeust.response.data);
+                //console.log("Errors ", errors);
+                setError("Дані вказано не вірно");
+
+            });
         //setData({email: "pylyp", password: "123456"});
     }
 
@@ -55,31 +58,37 @@ const LoginPage = () => {
         //console.log(e.target.name, e.target.value);
         setData({...data, [e.target.name]: e.target.value});
     }
-  return (
-    <>
-      <h1 className="text-center">Вхід на сайт</h1>
-      <form onSubmit={onSubmitHandler} className="col-md-6 offset-md-3">
-        <InputGroup
-           label="Електронна адреса"
-           field="email"
-           value={data.email}
-           onChange={onChangeHandler}
-        />
+    return (
+        <>
+            <h1 className="text-center">Вхід на сайт</h1>
 
-        <InputGroup
-          label="Пароль"
-          type="password"
-          field="password"
-          value={data.password}
-          onChange={onChangeHandler}
-        />
+            <form onSubmit={onSubmitHandler} className="col-md-6 offset-md-3">
+                {!!error &&
+                    <div className="alert alert-danger" role="alert">
+                        {error}
+                    </div>
+                }
+                <InputGroup
+                    label="Електронна адреса"
+                    field="email"
+                    value={data.email}
+                    onChange={onChangeHandler}
+                />
 
-        <button type="submit" className="btn btn-primary">
-          Вхід
-        </button>
-      </form>
-    </>
-  );
+                <InputGroup
+                    label="Пароль"
+                    type="password"
+                    field="password"
+                    value={data.password}
+                    onChange={onChangeHandler}
+                />
+
+                <button type="submit" className="btn btn-primary">
+                    Вхід
+                </button>
+            </form>
+        </>
+    );
 };
 
 export default LoginPage;
